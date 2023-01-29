e284ba95c008f1a976e565655e7a0dc3d9ea232c
athiraIsam <athira_isam94@yahoo.com>
2023-01-29 19:45:23 +0800

    Add project files.

---
+++ exchange_rate/ConversionHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exchange_rate
+{
+    public static class ConversionHelper
+    {
+        public static void convertDollars(double dollar, double convertRate, out double converted)
+        {
+            converted = dollar * convertRate;
+        }
+
+        public static void assignRate(int choice, out double conversionRate)
+        {
+            conversionRate = 0;
+            switch (choice)
+            {
+                case 1:
+                    conversionRate = .63071;
+                    break;
+                case 2:
+                    conversionRate = 14.0043;
+                    break;
+                case 3:
+                    conversionRate = 70.4800;
+                    break;
+                default:
+                    Console.WriteLine("Invalid input, try again");
+                    break;
+            }
+        }
+
+        public static void displayMenu()
+        {
+            Console.WriteLine("Please select one.");
+
+            Console.WriteLine("1. British Pounds");
+            Console.WriteLine("2. Mexican Pesos");
+            Console.WriteLine("3. Japanase Yen");
+            Console.WriteLine("4. Exit Program\n");
+        }
+    }
+}
---
+++ exchange_rate/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace exchange_rate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===============American Dollars Rate Converter===============\n");
+
+            //declare variable
+            int menuChoice = 0;
+            double americanDollars = 0.0;
+            double conversionRate = 0.0;
+            double convertedCurrency = 0.0;
+
+            ConversionHelper.displayMenu();
+
+            Console.Write("Your choice is:");
+            var strMenuChoice =Console.ReadLine();
+
+            try
+            {
+                menuChoice = int.Parse(strMenuChoice);
+
+                while (menuChoice > 0 && menuChoice < 4)
+                {
+                    Console.Write("\nNumber of American dollars:");
+                    var input = Console.ReadLine();
+
+                    if (Double.TryParse(input, out americanDollars))
+                    {
+                        ConversionHelper.assignRate(menuChoice, out conversionRate);
+
+                        ConversionHelper.convertDollars(americanDollars, conversionRate, out convertedCurrency);
+
+                        Console.WriteLine($"USD: {convertedCurrency}\n");
+
+                        ConversionHelper.displayMenu();
+
+                        input = Console.ReadLine();
+
+                        if (!Int32.TryParse(input, out menuChoice))
+                        {
+                            Console.WriteLine("No, input could not be parsed.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No, input could not be parsed.");
+                    }
+
+                   
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Invalid Input");
+            }
+           
+        }
+
+    
+    }
+}
